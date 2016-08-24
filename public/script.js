643638c0038a09d72e249ccb0758732a7d615fa3
Millie Walsh <millie@primeacademy.io>
2016-08-24 08:12:19 -0500

    test a few other apis, still broken

---
+++ public/script.js
@@ -0,0 +1,28 @@
+console.log('sourced');
+
+var getMe = function() {
+  FB.api('/me', {fields: 'last_name'}, function(response) {
+    console.log(response);
+  });
+};
+
+var getMyFeed = function(){
+  FB.api('/me/feed', function(response) {
+    if (!response || response.error) {
+      console.log(response.error);
+    } else {
+      console.log(response);
+    }
+  });
+};
+
+var getMyFriends = function(){
+  FB.api(
+      "/me/friends",
+      function (response) {
+        if (response && !response.error) {
+          console.log(response);
+        }
+      }
+  );
+};
